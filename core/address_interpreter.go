cb7d3678b5419fd12574076d145b3596038050f1
xiiv13 <937846472@qq.com>
2022-07-29 14:20:03 +0800

    TODO Mutex; TODO pre-format; TODO test_compete_next

--- core/address_interpreter.go
+++ core/address_interpreter.go
@@ -4,6 +4,7 @@ import (
 	"address_match_recommend/index"
 	. "address_match_recommend/models"
 	"regexp"
+	"strings"
 )
 
 // AddressInterpreter 地址解析操作, 从地址文本中解析出省、市、区、街道、乡镇、道路等地址组成部分
@@ -22,20 +23,18 @@ var (
 	specialChars1 = []byte("　 \r\n\t,，。·.．;；:：、！@$%*^`~=+&'\"|_-\\/")
 	// 特殊字符2
 	specialChars2 = []byte(`{}【】〈〉<>[]「」“”（）()`)
+
 	// 匹配没有路号的情况
-	P_BUILDING_NUM0 = regexp.MustCompile(`((路|街|巷)\d+号([\dA-Z一二三四五六七八九十][\\#\\-一－—/\\\\]|楼)?)?([0-9A-Z一二三四五六七八九十]+(栋|橦|幢|座|号楼|号|楼|\\#楼?)){0,1}([一二三四五六七八九十东西南北甲乙丙0-9]+([\\#\\-一－—/\\\\]|单元|门|梯|层|座|组))?([0-9]+([\\#\\-一－—/\\\\]|室|房|层|楼|号|户)?)?([0-9]+号?)?`)
+	reBuildingNum0 = regexp.MustCompile(`((路|街|巷)\d+号([\dA-Z一二三四五六七八九十][\\#\\-一－—/\\\\]|楼)?)?([\dA-Z一二三四五六七八九十]+(栋|橦|幢|座|号楼|号|楼|\\#楼?))?([一二三四五六七八九十东西南北甲乙丙\d]+([\\#\\-一－—/\\\\]|单元|门|梯|层|座|组))?(\d+([\\#\\-一－—/\\\\]|室|房|层|楼|号|户)?)?(\d+号?)?`)
 
-	/**
-	标准匹配building的模式：xx栋xx单元xxx
-	注1：山东青岛市南区宁夏路118号4号楼6单元202。如果正则模式开始位置不使用(路[0-9]+号)?, 则第一个符合条件的匹配结果是【118号4】,
-	按照逻辑会将匹配结果及之后的所有字符当做building，导致最终结果为：118号4号楼6单元202
-	所以需要先匹配 (路[0-9]+号)?
-	*/
-
-	P_BUILDING_NUM1 = regexp.MustCompile(`((路|街|巷)\d+号)?([0-9A-Z一二三四五六七八九十]+(栋|橦|幢|座|号楼|号|\\#楼?)){0,1}([一二三四五六七八九十东西南北甲乙丙0-9]+(单元|门|梯|层|座))?([0-9]+(室|房)?)?`)
+	// 标准匹配building的模式：xx栋xx单元xxx
+	// 山东青岛市南区宁夏路118号4号楼6单元202。如果正则模式开始位置不使用(路[0-9]+号)?, 则第一个符合条件的匹配结果是【118号4】,
+	// 按照逻辑会将匹配结果及之后的所有字符当做building，导致最终结果为：118号4号楼6单元202
+	// 所以需要先匹配 (路\d+号)?
+	reBuildingNum1 = regexp.MustCompile(`((路|街|巷)\d+号)?([\dA-Z一二三四五六七八九十]+(栋|橦|幢|座|号楼|号|\\#楼?))?([一二三四五六七八九十东西南北甲乙丙\d]+(单元|门|梯|层|座))?(\d+(室|房)?)?`)
 
 	// 校验building的模式。building1M能够匹配到纯数字等不符合条件的文本，使用building1V排除掉
-	P_BUILDING_NUM_V = regexp.MustCompile(`(栋|幢|橦|号楼|号|\\#|\\#楼|单元|室|房|门)+`)
+	reBuildingNumV = regexp.MustCompile(`(栋|幢|橦|号楼|号|\\#|\\#楼|单元|室|房|门)+`)
 
 	// 匹配building的模式：12-2-302，12栋3单元302
 	P_BUILDING_NUM2 = regexp.MustCompile(`[A-Za-z\d]+([\\#\\-一－/\\\\]+[A-Za-z\d]+)+`)
@@ -109,35 +108,68 @@ func (ai AddressInterpreter) interpret(entity *Address, visitor RegionInterprete
 	*/
 }
 
-// TODO
-
 // 清洗下开头垃圾数据
 func (ai AddressInterpreter) prepare(entity *Address) {
 	if len(entity.AddressText) == 0 {
 		return
 	}
-	// 去除开头的数字, 字母, 空格等
-	//p, _ := regexp.Compile("[ \\da-zA-Z\r\n\t,，。·.．;；:：、！@$%*^`~=+&'\"|_\\-\\/]")
 
-	/**
-	  entity.text = entity.text?.trimStart {
-	      p.matcher("$it").find()
-	  }
+	// 去除开头的数字, 字母, 空格, 符号
+	prefix := regexp.MustCompile("[ \\da-zA-Z\r\n\t,，。·.．;；:：、！@$%*^`~=+&'\"|_\\-\\/]")
+	strings.TrimPrefix(entity.AddressText, string(prefix.Find([]byte(entity.AddressText))))
 
-	  // 将地址中的 ー－—- 等替换为-
-	  entity.text = entity.text?.replace(Regex("[ー_－—/]|(--)"), "-")
-	*/
+	// 将地址中的 ー－—- 等替换为-
+	replace := regexp.MustCompile(`[ー_－—/]|(--)`)
+	replace.ReplaceAll([]byte(entity.AddressText), []byte("-"))
 }
 
 // 提取建筑物号
-func (ai AddressInterpreter) extractBuildingNum(entity *Address) bool {
+func (ai AddressInterpreter) extractBuildingNum(entity *Address) {
 	if len(entity.AddressText) == 0 {
-		return false
+		return
 	}
 	found := false // 是否找到的标志
-	//var building string // 最后匹配的文本
+	matches := reBuildingNum0.FindAllStringSubmatch(entity.AddressText, -1)
+	matchesIdx := reBuildingNum0.FindAllStringSubmatchIndex(entity.AddressText, -1)
+	for i, match := range matches {
+		if len(match[0]) == 0 {
+			continue
+		}
 
-	return found
+		var notEmptyCnt int
+		for _, v := range match {
+			if len(v) > 0 {
+				notEmptyCnt++
+			}
+		}
+
+		build := match[0]
+		if notEmptyCnt > 3 && reBuildingNumV.MatchString(build) {
+			pos := matchesIdx[i][0]
+			if strings.HasPrefix(build, "路") || strings.HasPrefix(build, "街") ||
+				strings.HasPrefix(build, "巷") {
+				if strings.Contains(build, "号楼") {
+					pos += strings.Index(build, "路") + 1
+				} else {
+					pos += strings.Index(build, "号") + 1
+				}
+				build = entity.AddressText[pos:matchesIdx[i][1]]
+			}
+			entity.BuildingNum = build
+			entity.AddressText = entity.AddressText[:pos] + entity.AddressText[matchesIdx[i][1]:]
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		matches := reBuildingNum1.FindAllStringSubmatch(entity.AddressText, -1)
+		matchesIdx := reBuildingNum1.FindAllStringSubmatchIndex(entity.AddressText, -1)
+		for i, v := range matches {
+			if l
+		}
+
+	}
 }
 
 //func interprets(addrTextList []string, visitor RegionInterpreterVisitor) []Address {
